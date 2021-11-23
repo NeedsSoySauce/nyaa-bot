b0c6957a2b15e779e55bf787d3f8d7958c609963
NeedsSoySauce <feras.albaroudi@gmail.com>
2021-11-24 00:20:06 +1300

    Refactor command deployment script

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
 import { Intents } from 'discord.js';
 import { MongoClient } from 'mongodb';
+import { EOL } from 'node:os';
 import { CommandClient } from './commandClient.js';
-import { DebugSlashCommand, SearchSlashCommand } from './commands/index.js';
+import { DebugSlashCommand, SearchSlashCommand, SlashCommand } from './commands/index.js';
 import { getEnvironmentVariable } from './env.js';
 import { CommandRepository, DatabaseCommandRepository } from './services/commandRepository.js';
 import { DatabaseUserRepository, UserRepository } from './services/userRepository.js';
@@ -10,12 +13,34 @@ const databaseUrl = getEnvironmentVariable("DATABASE_URL")
 const mongoClient = new MongoClient(databaseUrl);
 const db = mongoClient.db("nyaaDB");
 
-const main = async () => {
-  const userRepository: UserRepository = new DatabaseUserRepository(db)
-  const commandRepository: CommandRepository = new DatabaseCommandRepository(db)
+const deployCommands = async (commands: SlashCommand[]) => {
+  const applicationId = getEnvironmentVariable('DISCORD_APPLICATION_ID');
+  const guildId = getEnvironmentVariable('DISCORD_GUILD_ID');
 
-  const slashCommands = [new DebugSlashCommand(), new SearchSlashCommand(userRepository, commandRepository)]
+  const rest = new REST({ version: '9' }).setToken(getEnvironmentVariable('DISCORD_TOKEN'));
 
+    try {
+      console.log('Started refreshing application (/) commands.');
+
+      const builders = commands.map(c => c.createBuilder())
+
+      console.log(`Found ${commands.length} command(s):`)
+      console.log(`${builders.map(builder => `/${ builder.name}`).join(EOL)}`)
+
+      await rest.put(
+        Routes.applicationGuildCommands(applicationId, guildId),
+        { body: builders.map(builder => builder.toJSON()) },
+      );
+
+      console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+      console.error(error);
+    }
+    // process.exit(0)
+    return Promise.resolve()
+}
+
+const startCommandClient = (commands: SlashCommand[]) => {
   const commandClient = new CommandClient({
       intents: [
           Intents.FLAGS.GUILDS,
@@ -25,16 +50,29 @@ const main = async () => {
           Intents.FLAGS.GUILD_MESSAGE_REACTIONS
       ],
       partials: ["CHANNEL"],
-      commands: slashCommands
+      commands
   });
 
   commandClient.login(process.env.DISCORD_TOKEN);
+}
+
+const main = async () => {
+  const userRepository: UserRepository = new DatabaseUserRepository(db)
+  const commandRepository: CommandRepository = new DatabaseCommandRepository(db)
+
+  const commands = [new DebugSlashCommand(), new SearchSlashCommand(userRepository, commandRepository)]
+
+  if (process.argv.some(value => value === 'deploy-commands')) {
+    await deployCommands(commands)
+  } else {
+    startCommandClient(commands);
+  }
 }
 
 mongoClient.connect(async err => {
   if (err) {
     console.error(err)
   } else {
-    main()
+    await main()
   }
 });
